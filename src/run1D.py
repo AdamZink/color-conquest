eb2bcf6240b8fd236f9a6fcebac536f9cffcee97
Adam Zink <adamzink55@gmail.com>
2018-01-13 11:12:05 -0500

    Modify simulation program to use grid util

--- src/run1D.py
+++ src/run1D.py
@@ -1,3 +1,4 @@
+from grid_util import GridUtil as gutil
 from neighborhood_util import NeighborhoodUtil as nutil
 import numpy as np
 
@@ -6,32 +7,20 @@ gridHueList = [0, 120]
 maxN = 2
 
 layers = np.zeros(shape=(len(gridHueList), gridColumns))
-layers[0][0] = 1
-layers[1][1] = 1
 layers[1][2] = 1
 
 print('\nInitial:')
 print(layers)
 
-layer1 = layers[0]
-layer2 = layers[1]
+for hueIndex in range(len(gridHueList)):
+	for layerColumn in range(gridColumns):
+		descriptionList = nutil.describeEncodedNeighborhood(
+			gutil.getOpposingEncodedNeighborhood(layers, hueIndex, layerColumn, maxN, buckets=2),
+			maxN, buckets=2
+		)
 
-print('\nLayer 1 - check for units in layer 2')
-for i in range(layer1.size):
-	if (layer1[i] > 0):
-		print('i = ' + str(i))
-		print(nutil.describeEncodedNeighborhood(
-			nutil.getEncodedNeighborhood(layer2, i, maxN),
-			maxN
-		))
-
-print('\nLayer 2 - check for units in layer 1')
-for j in range(layer2.size):
-	if (layer2[j] > 0):
-		print('j = ' + str(j))
-		print(nutil.describeEncodedNeighborhood(
-			nutil.getEncodedNeighborhood(layer1, j, maxN),
-			maxN
-		))
+		if (len(descriptionList) > 0):
+			print('Hue index ' + str(hueIndex) + ', layer column index ' + str(layerColumn) + ':')
+			print(descriptionList)
 
 print('\nFinished simulation')
